129ba238ac111fdb4324252029cd57366af699bc
pipka69 <artem.dulepov_228@mail.ru>
2026-06-12 12:56:06 +0000

    minimal interface added

--- app/main.py
+++ app/main.py
@@ -1,16 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
 import uvicorn
 from routers.todos import router as todos_router
+# импорты для фронтенда
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 app.include_router(todos_router)
 
+# папка с html и css
+templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-@app.get("/", tags=['HelloWorld_root'])
-async def root():
-    return {"message": "Hello World"}
 
+# 
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(request, "index.html", {})
 
 
 
